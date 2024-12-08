d6533df6207eabd7e5a283e7e0133e880abf23e1
Saloni Saroha <salonisaroha5@gmail.com>
2024-12-08 18:12:00 +0530

    Adding REST Api by following the MVC pattern

---
+++ services/user-service.go
@@ -0,0 +1,10 @@
+package services
+
+import "Salonisaroha/models"
+type UserService interface{
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([] *models.User, error)
+	UpdateUser(*models.User) error
+	DeleteUser(*string) error
+}
---
+++ services/user.service.impl.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"Salonisaroha/models"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserServiceImpl struct {
+	usercollection *mongo.Collection
+	ctx             context.Context
+}
+
+func NewUserService(usercollection *mongo.Collection, ctx context.Context ) UserService{
+	return &UserServiceImpl{
+     usercollection: usercollection,
+	 ctx:            ctx,
+	}
+}
+func(u *UserServiceImpl) CreateUser(user *models.User) error{
+	_, err :=u.usercollection.InsertOne(u.ctx, user)
+	return err
+}
+func(u *UserServiceImpl) GetUser(name *string) (*models.User, error){
+	var user *models.User
+	query := bson.D{bson.E{Key: "name", Value:name}}
+	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
+}
+
+func(u *UserServiceImpl) GetAll() ([]*models.User, error){
+	return nil, nil
+}
+func(u *UserServiceImpl) UpdateUser(user *models.User) error {
+	return nil
+}
+func(u *UserServiceImpl) DeleteUser(user *string) error {
+	return nil
+}
